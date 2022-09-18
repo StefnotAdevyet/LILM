eda95e221339104fa3bf492063f5ea2a4a51aa68
StefnotAdevyet <steph.jackson556@gmail.com>
2022-09-18 12:13:22 -0400

    added base project

---
+++ LILM/LILM/LILMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LILMApp.swift
+//  LILM
+//
+//  Created by developer on 9/18/22.
+//
+
+import SwiftUI
+
+@main
+struct LILMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
